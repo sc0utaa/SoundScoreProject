e2d88cf1d8a859cada898e3f0400988ee33f9a31
Shmiril2 <kiril.todorov19@gmail.com>
2025-02-20 20:33:50 +0200

    Add Album view and migrations

--- SoundScoreProject/Program.cs
+++ SoundScoreProject/Program.cs
@@ -1,7 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using SoundScore.Data;
+using SoundScore.Services.Abstractions;
+using SoundScore.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IMusicBrainzService, MusicBrainzService>();
 
 var app = builder.Build();
 
