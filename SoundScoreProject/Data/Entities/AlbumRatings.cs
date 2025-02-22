e62eb6a44e25a830c6043f38b1337118592e2c9d
sc0utaa <petar.kashlov@gmail.com>
2025-02-22 13:33:22 +0200

    design

---
+++ SoundScoreProject/Data/Entities/AlbumRatings.cs
@@ -0,0 +1,9 @@
+﻿namespace SoundScoreProject.Data.Entities
+{
+    public class AlbumRatings
+    {
+        public int ID { get; set; }
+        public int AlbumID { get; set; }
+        public int Rating { get; set; }
+    }
+}
